683d628d025db46e91d62f010baa63c96cf202db
Serhii Polishchuk <spolischook@gmail.com>
2023-01-14 21:00:06 +0100

    Try disable CGO

--- go/src/api/lambda/build.sh
+++ go/src/api/lambda/build.sh
@@ -3,5 +3,5 @@ set -xe
 
 go env -w GOPRIVATE=github.com/BajaBikes
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o main api/lambda/lambda.go
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o main api/lambda/lambda.go
 zip deployment.zip main .env
