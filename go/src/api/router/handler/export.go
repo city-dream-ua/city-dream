531e39c7ac372a58684bd4b15d4198555ece8ad4
Serhii Polishchuk <spolischook@gmail.com>
2023-02-03 00:30:25 +0100

    Updated api

--- go/src/api/router/handler/export.go
+++ go/src/api/router/handler/export.go
@@ -8,6 +8,7 @@ import (
 	"github.com/kpango/glg"
 
 	"github.com/City-Dream/backend/api/export"
+	vercelDeploy "github.com/City-Dream/backend/deploy/vercel"
 	"github.com/City-Dream/backend/model/migration"
 	trelloImport "github.com/City-Dream/backend/trello/import"
 )
@@ -34,6 +35,15 @@ func ExportStaticApi(c *gin.Context) {
 	if err != nil {
 		glg.Errorf("export static api error: %s", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("export static api error: %s", err)})
+		return
+	}
+
+	glg.Info("run vercel deploy")
+	err = vercelDeploy.Do()
+	if err != nil {
+		err = fmt.Errorf("vercel deploy error: %s", err)
+		glg.Errorf(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
