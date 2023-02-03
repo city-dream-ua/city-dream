965009c88738e9817ca9c655972f6c6fc5e99451
Serhii Polishchuk <spolischook@gmail.com>
2023-02-03 01:26:55 +0100

    Run migrations before import

--- go/src/cmd/trello/import/main.go
+++ go/src/cmd/trello/import/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/City-Dream/backend/model/migration"
 	trelloImport "github.com/City-Dream/backend/trello/import"
 )
 
 
 func main() {
+	err := migration.Do()
+	checkErr(err)
 	checkErr(trelloImport.Do())
 }
 
